b53ee405524f77a62de1cd613ec4328f001895ac
RakibulHossainHimel <himel577303@gmail.com>
2025-07-11 05:36:56 +0600

    one page of medicine almost all the details have been scraped

--- Scraping_MedEx/pipelines.py
+++ Scraping_MedEx/pipelines.py
@@ -12,15 +12,5 @@ import html
 class ScrapingMedexPipeline:
     def process_item(self, item, spider):
       item["Unit_Price_BDT"] = float(item["Unit_Price_BDT"].replace('৳', '').replace(',', '').strip()) 
-      raw_html= item["Indications"]
       
-    # Step 1: Replace <br> tags with newline
-      raw_html = raw_html.replace("<br>", "\n").replace("<br/>", "\n")
-
-        # Step 2: Remove HTML tags
-      text = remove_tags(raw_html)
-
-    # Step 3: Unescape HTML entities and strip extra spaces
-      clean_text = html.unescape(text).strip()
-      item["Indications"] = clean_text
       return item
--- Scraping_MedEx/spiders/MedEx.py
+++ Scraping_MedEx/spiders/MedEx.py
@@ -34,9 +34,141 @@ class MedexSpider(scrapy.Spider):
         product['Generic_name'] = response.xpath("//div[@title='Generic Name']/a/text()").get().strip()
         product['Strength'] = response.xpath("//div[@title='Strength']/text()").get().strip()
         product['Manufactured_by'] = response.xpath("//div[@title='Manufactured by']/a/text()").get().strip()
+        #product['Marketed_by'] = response.xpath("//div[@title='Marketed by']/a/text()").get().strip()
         product['Unit_Price_BDT'] = response.xpath("//div[@class='package-container mt-5 mb-5']/span[2]/text()").get().strip()
-        if response.xpath("//div[@id='indications']/h3/text()").get()=="Indications":
-            product['Indications'] = response.xpath("//div[@class='ac-body']").get().strip()
         
+        #Antibiotic
+        product['IsAntibiotic'] = "Yes" if "Antibiotic" in " ".join(t.strip() for t in response.xpath("//div[@class='mb-2']//text()").getall() if t.strip()) else "No"
+
+
+        #Indications
+        if response.xpath("//div[@id='indications']/h3/text()").get()=="Indications":
+            Indications_text_list = response.xpath("//div[@id='indications']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Indications_full_text = " ".join([text.strip() for text in Indications_text_list if text.strip()])
+            product['Indications'] = Indications_full_text
+        else:
+            product['Indications'] = ""
+
+        #Composition
+        if response.xpath("//div[@id='composition']/h3/text()").get()=="Composition":
+            Composition_text_list = response.xpath("//div[@id='composition']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Composition_full_text = " ".join([text.strip() for text in Composition_text_list if text.strip()])
+            product['Composition'] = Composition_full_text
+        else:
+            product['Composition'] = ""
+        
+        #Pharmacology
+        if response.xpath("//div[@id='mode_of_action']/h3/text()").get()=="Pharmacology":
+            Pharmacology_text_list = response.xpath("//div[@id='mode_of_action']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Pharmacology_full_text = " ".join([text.strip() for text in Pharmacology_text_list if text.strip()])
+            product['Pharmacology'] = Pharmacology_full_text
+        else:
+            product['Pharmacology'] = ""
+
+        #Dosage & Administration
+        if response.xpath("//div[@id='dosage']/h3/text()").get()=="Dosage & Administration":
+            Dosage_and_Administration_text_list = response.xpath("//div[@id='dosage']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Dosage_and_Administration_full_text = " ".join([text.strip() for text in Dosage_and_Administration_text_list if text.strip()])
+            product['Dosage_and_Administration'] = Dosage_and_Administration_full_text
+        else:
+            product['Dosage_and_Administration'] = ""
+
+        #Interaction
+        if response.xpath("//div[@id='interaction']/h3/text()").get()=="Interaction":
+            Interaction_text_list = response.xpath("//div[@id='indications']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Interaction_full_text = " ".join([text.strip() for text in Interaction_text_list if text.strip()])
+            product['Interaction'] = Interaction_full_text
+        else:
+            product['Interaction'] = ""
+
+        #Contraindications
+        if response.xpath("//div[@id='contraindications']/h3/text()").get()=="Contraindications":
+            Contraindications_text_list = response.xpath("//div[@id='contraindications']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Contraindications_full_text = " ".join([text.strip() for text in Contraindications_text_list if text.strip()])
+            product['Contraindications'] = Contraindications_full_text
+        else:
+            product['Contraindications'] = ""
+        
+        #Side Effects
+        if response.xpath("//div[@id='side_effects']/h3/text()").get()=="Side Effects":
+            Side_Effects_text_list = response.xpath("//div[@id='side_effects']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Side_Effects_full_text = " ".join([text.strip() for text in Side_Effects_text_list if text.strip()])
+            product['Side_Effects'] = Side_Effects_full_text
+        else:
+            product['Side_Effects'] = ""
+
+        #Pregnancy & Lactation
+        if response.xpath("//div[@id='pregnancy_cat']/h3/text()").get()=="Pregnancy & Lactation":
+            Pregnancy_and_Lactation_text_list = response.xpath("//div[@id='pregnancy_cat']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Pregnancy_and_Lactation_full_text = " ".join([text.strip() for text in Pregnancy_and_Lactation_text_list if text.strip()])
+            product['Pregnancy_and_Lactation'] = Pregnancy_and_Lactation_full_text
+        else:
+            product['Pregnancy_and_Lactation'] = ""
+
+        #Precautions & Warnings
+        if response.xpath("//div[@id='precautions']/h3/text()").get()=="Precautions & Warnings":
+            Precautions_and_Warnings_text_list = response.xpath("//div[@id='precautions']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Precautions_and_Warnings_full_text = " ".join([text.strip() for text in Precautions_and_Warnings_text_list if text.strip()])
+            product['Precautions_and_Warnings'] = Precautions_and_Warnings_full_text
+        else:
+            product['Precautions_and_Warnings'] = ""
+
+        #pediatric Uses
+        if response.xpath("//div[@id='pediatric_uses']/h3/text()").get()=="Use in Special Populations":
+            pediatric_Uses_text_list = response.xpath("//div[@id='pediatric_uses']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            pediatric_Uses_full_text = " ".join([text.strip() for text in pediatric_Uses_text_list if text.strip()])
+            product['pediatric_Uses'] = pediatric_Uses_full_text
+        else:
+            product['pediatric_Uses'] = ""
+
+        #Overdose Effects
+        if response.xpath("//div[@id='overdose_effects']/h3/text()").get()=="Overdose Effects":
+            Overdose_Effects_text_list = response.xpath("//div[@id='overdose_effects']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Overdose_Effects_full_text = " ".join([text.strip() for text in Overdose_Effects_text_list if text.strip()])
+            product['Overdose_Effects'] = Overdose_Effects_full_text
+        else:
+            product['Overdose_Effects'] = ""
+        
+        #Therapeutic Class
+        if response.xpath("//div[@id='drug_classes']/h3/text()").get()=="Therapeutic Class":
+            Therapeutic_Class_text_list = response.xpath("//div[@id='drug_classes']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Therapeutic_Class_full_text = " ".join([text.strip() for text in Therapeutic_Class_text_list if text.strip()])
+            product['Therapeutic_Class'] = Therapeutic_Class_full_text
+        else:
+            product['Therapeutic_Class'] = ""
+
+        #Reconstitution
+        if response.xpath("//div[@id='reconstitution']/h3/text()").get()=="Reconstitution":
+            Reconstitution_text_list = response.xpath("//div[@id='reconstitution']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Reconstitution_full_text = " ".join([text.strip() for text in Reconstitution_text_list if text.strip()])
+            product['Reconstitution'] = Reconstitution_full_text
+        else:
+            product['Reconstitution'] = ""
+        
+        #Storage Conditions
+        if response.xpath("//div[@id='storage_conditions']/h3/text()").get()=="Storage Conditions":
+            Storage_Conditions_text_list = response.xpath("//div[@id='storage_conditions']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Storage_Conditions_full_text = " ".join([text.strip() for text in Storage_Conditions_text_list if text.strip()])
+            product['Storage_Conditions'] = Storage_Conditions_full_text
+        else:
+            product['Storage_Conditions'] = ""
+
+        #Chemical Structure
+        if response.xpath("//div[@id='compound_summary']/h3/text()").get()=="Chemical Structure":
+            Chemical_Structure_text_list = response.xpath("//div[@id='compound_summary']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Chemical_Structure_full_text = " ".join([text.strip() for text in Chemical_Structure_text_list if text.strip()])
+            product['Chemical_Structure'] = Chemical_Structure_full_text
+        else:
+            product['Chemical_Structure'] = ""
+
+        #Common_Questions
+        if response.xpath("//div[@id='commonly_asked_questions']/h3/text()").get() and 'Common Questions' in response.xpath("//div[@id='commonly_asked_questions']/h3/text()").get():
+        
+            Common_Questions_text_list = response.xpath("//div[@id='commonly_asked_questions']/following-sibling::div[@class='ac-body'][1]//text()").getall()
+            Common_Questions_full_text = " ".join([text.strip() for text in Common_Questions_text_list if text.strip()])
+            product['Common_Questions'] = Common_Questions_full_text
+        else:
+            product['Common_Questions'] = ""
+
 
         yield product
